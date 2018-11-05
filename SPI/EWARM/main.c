8859f6f6c4d108b2a471f9b7693fe955cb51f64c
burche1 <burche.t.d@gmail.com>
2018-11-05 19:33:12 -0200

    SPI implementation

---
+++ SPI/EWARM/main.c
@@ -0,0 +1,13 @@
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_usart.h"
+#include "misc.h"
+#include "string.h"
+#include "stm32f10x_spi.h"
+
+int main()
+{
+  return 0;
+}
