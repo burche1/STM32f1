6c1f093376b12daf122de5edacbca5320f422815
burche1 <burche.t.d@gmail.com>
2018-11-23 06:51:42 -0200

    I2C_ST_EEPROM

--- I2C/EWARM/main.c
+++ I2C/EWARM/main.c
@@ -8,6 +8,7 @@
 #include "stm32f10x_i2c.h"
 #include "stdio.h"
 
+
 /* Macro ----------------------------------------------------------------------*/
 #define B(x) S_to_binary_(#x)
 
@@ -22,16 +23,24 @@ static inline unsigned long long S_to_binary_(const char *s)
 }
 
 
+/* Defines --------------------------------------------------------------------*/
+#define TIMEOUT                 ((uint32_t)0x1000)
+#define LONG_TIMEOUT            ((uint32_t)(10 * TIMEOUT))
+
+
 /* Variables ------------------------------------------------------------------*/
 uint8_t SlaveAddress; //E0, E1, E2 to ground
-uint8_t Read;
-uint8_t Write;
 uint8_t TempRead;
 uint8_t ReadValue;
-uint8_t Data;
+__IO uint32_t  timeout = LONG_TIMEOUT;
 
 
 /* Functions ------------------------------------------------------------------*/
+uint32_t CallBack(void)
+{
+  while(1);
+}
+
 
 void I2C1_init(void)
 {
@@ -61,45 +70,72 @@ void I2C1_init(void)
     I2C_Init(I2C1, &I2C_InitStructure);
 }
 
-void I2C_Write(uint8_t DataToSend)
+
+uint32_t I2C_Write(uint8_t* DataToSend, uint16_t WriteAddr, uint8_t NumByteToWrite)
 {
+  /*!< Address of EEPROM */
   SlaveAddress = B(1010000);
-  Write = B(0);
+  
+  /*!< While the bus is busy */
+  timeout = LONG_TIMEOUT;
+  while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY))
+  {
+    if((timeout--)==0) return CallBack();
+  }
+  
   /* Start Condition */
   I2C_GenerateSTART(I2C1, ENABLE);
+  
+  /*!< Test on EV5 and clear it */
+  timeout = TIMEOUT;
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT))
+  {
+    if((timeout--) == 0) return CallBack();
+  }
+  
+  timeout = TIMEOUT;
   /* Slave Address Transmitter */
   I2C_Send7bitAddress(I2C1, SlaveAddress, I2C_Direction_Transmitter);
-  /* Read or Write */
-  I2C_SendData(I2C1, Write);
-  /* Ack */
-  if (I2C_ReceiveData(I2C1) == B(0))
-  {
-    /* Data */
-    I2C_SendData(I2C1, DataToSend);
-    TempRead = B(10000000);        //código de sucesso da escrita
-  }
-  else
-    TempRead = B(00001111);        //código de erro Address e R/W
   
-  if (I2C_ReceiveData(I2C1) == B(1))
+  /*!< Test on EV6 and clear it */
+  timeout = TIMEOUT;
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED))
   {
-    /* Stop Condition */ 
-    I2C_GenerateSTOP(I2C1, ENABLE);
+    if((timeout--) == 0) return CallBack();
   }
-  else
-    TempRead = B(00001110);        //código de erro Send Data
+  
+  /*!< Send the EEPROM's internal address to write to : only one byte Address */
+  I2C_SendData(I2C1, WriteAddr);
+  
+  /*!< Test on EV8 and clear it */
+  timeout = TIMEOUT;  
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED))
+  {
+    if((timeout--) == 0) return CallBack();
+  }  
+  
+  /*!< Send Data to be written */
+  I2C_SendData(I2C1, DataToSend);
+  
+  /*!< Test on EV8 and clear it */
+  timeout = TIMEOUT;  
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED))
+  {
+    if((timeout--) == 0) return CallBack();
+  }
+  
+  /* Stop Condition */
+  I2C_GenerateSTOP(I2C1, ENABLE);
 }
+
 
 int I2C_Read()
 {
   SlaveAddress = B(1010000);
-  Read = B(1);
   /* Start Condition */
   I2C_GenerateSTART(I2C1, ENABLE);
   /* Slave Address Receiver */
   I2C_Send7bitAddress(I2C1, SlaveAddress, I2C_Direction_Receiver);
-  /* Read or Write */
-  I2C_SendData(I2C1, Read);
   
   /* Ack Test */
   if (I2C_ReceiveData(I2C1) == B(1))
@@ -122,78 +158,18 @@ int I2C_Read()
   return TempRead;
 }
 
-/*
-  * @brief  Configures the specified I2C own address2.
-  * @param  I2Cx: where x can be 1 or 2 to select the I2C peripheral.
-  * @param  Address: specifies the 7bit I2C own address2.
-  * @retval None.
-I2C_OwnAddress2Config(I2C_TypeDef* I2Cx, uint8_t Address)
-*/
-
-
-/*
-  * @brief  Sends a data byte through the I2Cx peripheral.
-  * @param  I2Cx: where x can be 1 or 2 to select the I2C peripheral.
-  * @param  Data: Byte to be transmitted..
-  * @retval None
-I2C_SendData(I2C_TypeDef* I2Cx, uint8_t Data)
-*/
-
-
-/*
-  * @brief  Returns the most recent received data by the I2Cx peripheral.
-  * @param  I2Cx: where x can be 1 or 2 to select the I2C peripheral.
-  * @retval The value of the received data.
-I2C_ReceiveData(I2C_TypeDef* I2Cx)
-*/
-
-
-/*
-  * @brief  Transmits the address byte to select the slave device.
-  * @param  I2Cx: where x can be 1 or 2 to select the I2C peripheral.
-  * @param  Address: specifies the slave address which will be transmitted
-  * @param  I2C_Direction: specifies whether the I2C device will be a
-  *   Transmitter or a Receiver. This parameter can be one of the following values
-  *     @arg I2C_Direction_Transmitter: Transmitter mode
-  *     @arg I2C_Direction_Receiver: Receiver mode
-  * @retval None.
-I2C_Send7bitAddress(I2C_TypeDef* I2Cx, uint8_t Address, uint8_t I2C_Direction)
-*/
-
 
 int main()
 {
   I2C1_init();
-  uint8_t Data = B(01101001);        // i
+  uint8_t Data[] = 01101001;        // i
   TempRead = B(00000000);
-  /* Start Condition */
-  //I2C_GenerateSTART(I2C1, ENABLE);
-  
-  /* Slave Address Transmitter */
-  //I2C_Send7bitAddress(I2C1, SlaveAddress, I2C_Direction_Transmitter);
-  
-  /* Slave Address Receiver */
-  //I2C_Send7bitAddress(I2C1, SlaveAddress, I2C_Direction_Receiver);
-  
-  /* Read or Write */
-  //I2C_SendData(I2C1, Read);
-  //I2C_SendData(I2C1, Write);
-  
-  /* Ack */
-  //Ack
-  
-  /* Data */
-  //I2C_SendData(I2C1, Data);
-  
-  /* Ack */
-  //Ack
-  
-  /* Stop Condition */ 
-  //I2C_GenerateSTOP(I2C1, ENABLE);
+  uint8_t WriteAddr = B(00000000);
+  uint8_t NumByteToWrite = 1;
   
   while(1)
   {
-    I2C_Write(Data);
+    I2C_Write(Data, WriteAddr, NumByteToWrite);
     ReadValue = I2C_Read();
   }
 }
